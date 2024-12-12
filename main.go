3d79d6774027488be3e366c54387c94c64df38c2
RaptorExTolop <jamesappletonret@gmail.com>
2024-12-12 20:00:32 +1030

    Dev0.0.1: set up project - including DT

--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+var (
+	running bool
+
+	dt float32
+
+	windowHeight, windowWidth int32
+)
+
+func input() {}
+
+func update() {
+	running = !rl.WindowShouldClose()
+	dt = rl.GetFrameTime()
+}
+
+func draw() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Green)
+	rl.DrawText(fmt.Sprint("FPS: ", rl.GetFPS()), 0, 0, 24, rl.White)
+	rl.DrawText(fmt.Sprint("DT:  ", dt), 0, 28, 24, rl.White)
+
+	rl.EndDrawing()
+}
+
+func init() {
+	windowHeight = 720
+	windowWidth = 1280
+
+	rl.InitWindow(windowWidth, windowHeight, "Pong")
+	running = true
+}
+
+func quit() {
+	rl.CloseWindow()
+}
+
+func main() {
+	defer quit()
+
+	for running {
+		input()
+		update()
+		draw()
+	}
 }
