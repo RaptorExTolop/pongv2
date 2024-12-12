991542bef3cb87dd1a7e63c2b937f37acaae1bd2
RaptorExTolop <jamesappletonret@gmail.com>
2024-12-12 19:53:31 +1030

    Dev0.0.0: restarting because abstraction is bad

--- hitbox.go
+++
@@ -1,5 +0,0 @@
-package main
-
-type HitBox struct {
-	x1, x2, y1, y2 int32
-}
--- main.go
+++ main.go
@@ -1,41 +1,5 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
-
-type GameObject interface {
-	update()
-	draw()
-}
-
-type gameObjects []GameObject
-
-var (
-	running  bool = true
-	gameObjs gameObjects
-)
-
-func update() {
-	running = !rl.WindowShouldClose()
-}
-
-func draw() {
-	rl.BeginDrawing()
-
-	rl.ClearBackground(rl.RayWhite)
-	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
-	rl.EndDrawing()
-}
-
-func init() {
-	rl.SetTargetFPS(60)
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
-}
-
 func main() {
-	defer rl.CloseWindow()
-	for running {
-		update()
-		draw()
-	}
+
 }
