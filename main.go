0b524d1a0bd54776071c73f0de277050300ac6a8
RaptorExTolop <jamesappletonret@gmail.com>
2024-12-12 21:02:15 +1030

    Dev0.0.2: I HATE FOIWAFOPJKBFBUJWA FAWH FH UWAUI MATHS

--- main.go
+++ main.go
@@ -6,25 +6,42 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+type Player struct {
+	X      int32
+	Y      int32
+	Width  int32
+	Height int32
+	colour rl.Color
+}
+
 var (
-	running bool
+	// game setup
+	running                        bool
+	dt                             float32
+	windowHeight, windowWidth, fps int32
 
-	dt float32
-
-	windowHeight, windowWidth int32
+	player = Player{}
 )
 
 func input() {}
 
 func update() {
+	fps = rl.GetFPS()
 	running = !rl.WindowShouldClose()
 	dt = rl.GetFrameTime()
 }
 
 func draw() {
 	rl.BeginDrawing()
+
 	rl.ClearBackground(rl.Green)
-	rl.DrawText(fmt.Sprint("FPS: ", rl.GetFPS()), 0, 0, 24, rl.White)
+
+	rl.DrawRectangle(player.X, player.Y, player.Width, player.Height, player.colour)
+
+	/*
+		Debugging statments
+	*/
+	rl.DrawText(fmt.Sprint("FPS: ", fps), 0, 0, 24, rl.White)
 	rl.DrawText(fmt.Sprint("DT:  ", dt), 0, 28, 24, rl.White)
 
 	rl.EndDrawing()
@@ -36,6 +53,7 @@ func init() {
 
 	rl.InitWindow(windowWidth, windowHeight, "Pong")
 	running = true
+	player = Player{5, (windowHeight/2 - player.Height/2), 60, 180, rl.RayWhite}
 }
 
 func quit() {
